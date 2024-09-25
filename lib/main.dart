006b73db829db17da7d4e3b0d54a862e249da549
Jeongwon Kim <mickykim4@gmail.com>
2024-09-25 13:32:48 +0900

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
+import 'firebase_options.dart';
 import 'screens/main_screen.dart';
+import 'utils/theme/color_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  if (kDebugMode) {
+    try {
+      FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+    } catch (e) {
+      print(e);
+    }
+  }
   runApp(ProviderScope(child: App()));
 }
 
@@ -27,6 +42,7 @@ class _AppState extends State<App> {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       routerConfig: widget.router,
+      theme: ThemeData(scaffoldBackgroundColor: ColorManager.background),
     );
   }
 }
